b130f1f124c1b27888ceae14598cb3cd0752b0aa
Ilya <127449810+sumoomus@users.noreply.github.com>
2023-03-14 00:02:36 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int x = 10, y = 20;
+
+        double z = Math.sqrt (x*x + y*y);
+
+        System.out.println ( "hypotenuse = " + z );
+
+    }
+}
